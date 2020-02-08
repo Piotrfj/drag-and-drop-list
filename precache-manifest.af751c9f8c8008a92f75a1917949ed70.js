5c6cf489f442bc61b0ddc1aa0b9942e0ce228a56
Piotr Fijałkiewicz <piotr.fjj@gmail.com>
2020-02-08 20:17:38 +0100

    Updates

---
+++ precache-manifest.af751c9f8c8008a92f75a1917949ed70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edff0e7fcdf7ff9ef56a53c85b8274a4",
+    "url": "/drag-and-drop-list/index.html"
+  },
+  {
+    "revision": "01db159ee8047f5d6bef",
+    "url": "/drag-and-drop-list/static/css/main.f197bd6c.chunk.css"
+  },
+  {
+    "revision": "2cf10f9a1b766fa6b373",
+    "url": "/drag-and-drop-list/static/js/2.33ef5c84.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/drag-and-drop-list/static/js/2.33ef5c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01db159ee8047f5d6bef",
+    "url": "/drag-and-drop-list/static/js/main.f7d7db96.chunk.js"
+  },
+  {
+    "revision": "5164312a0a2c196f59df",
+    "url": "/drag-and-drop-list/static/js/runtime-main.e3671a4c.js"
+  },
+  {
+    "revision": "b7dde0cd8962d5b298a6fdba75950ab2",
+    "url": "/drag-and-drop-list/static/media/hamburger.b7dde0cd.svg"
+  }
+]);
